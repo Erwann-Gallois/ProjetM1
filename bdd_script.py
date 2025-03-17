b153ae64d57d354b62ba15cbf93eaa2cb5832446
Erwann-Gallois <gallois.erwann@gmail.com>
2025-03-17 11:19:00 +0100

    Ajout de fonctions pour récupérer les indicateurs et dialogues en fonction de la partie dans la base de données

--- bdd_script.py
+++ bdd_script.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import os
 import psycopg2
-import json
-import requests
-import morpho
+import numpy as np
+
 
 # 📌 Connexion PostgreSQL
 connexion = psycopg2.connect(
@@ -16,3 +14,49 @@ connexion = psycopg2.connect(
 curseur = connexion.cursor()
 
 # Indicateur 
+def get_indicateur(partie_de) : 
+    """
+    Permet de retourner les indicateurs de la base de données
+    partie_de : int
+    1 : Test
+    2 : Validation
+    3 : Train
+    """
+    if type(partie_de) != int : 
+        raise ValueError("La partie doit être un entier")
+    if partie_de > 3 or partie_de < 1 : 
+        raise ValueError("La partie doit être comprise entre 1 et 3")
+    
+    sql = """
+    SELECT indicateur FROM participants WHERE partof = %s
+    """
+    curseur.execute(sql, (partie_de,))
+    resultats = curseur.fetchall()
+
+    # Extraire les valeurs des tuples et les mettre dans une liste
+    indicateurs = [row[0] for row in resultats]
+    tableau_indicateurs = np.array(indicateurs)
+    return tableau_indicateurs
+
+def get_dialoguefr (partie_de) :
+    """
+    Permet de retourner les indicateurs de la base de données
+    partie_de : int
+    1 : Test
+    2 : Validation
+    3 : Train
+    """
+    if type(partie_de) != int : 
+        raise ValueError("La partie doit être un entier")
+    if partie_de > 3 or partie_de < 1 : 
+        raise ValueError("La partie doit être comprise entre 1 et 3")
+    
+    sql = """
+    SELECT dialogue_fr FROM participants WHERE partof = %s
+    """
+    curseur.execute(sql, (partie_de,))
+    resultats = curseur.fetchall()
+
+    # Extraire les valeurs des tuples et les mettre dans une liste
+    dialogues = [row[0] for row in resultats]
+    return dialogues
